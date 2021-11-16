5f446809297900cba2d82c4c0d0925b33aa12dd5
Maksym Demeshok <maksymdemeshok@MacBook-Pro-Maksym.local>
2021-11-16 21:53:51 +0100

    Adding DB

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash');
+const mongoose = require('mongoose');
+
+mongoose.connect(`mongodb://localhost:27017/blogDB`); 
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,8 +19,16 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+const postsSchema = mongoose.Schema({
+  title: String,
+  content: String,
+  link: String
+})
 
-let posts = []
+const Post = new mongoose.model('Post', postsSchema); 
+
+
+
 
 
 
@@ -26,7 +37,11 @@ let posts = []
 
 app.get('/', (req, res) => {
 
-  res.render('home', { homeStartingContent: homeStartingContent, posts: posts });
+  Post.find((err, posts)=>{
+    res.render('home', { homeStartingContent: homeStartingContent, posts: posts });
+  })
+  
+  
 
 })
 
@@ -47,14 +62,17 @@ app.get('/posts/:postName', (req, res) => {
 
   let requestTitle = _.kebabCase(req.params.postName);
 
-  posts.forEach(post => {
-    if(requestTitle === _.kebabCase(post.title))
-    res.render('post', { 
-      title: post.title, 
-      content: post.content 
+  Post.find((err, posts)=>{
+    posts.forEach(post => {
+      if(requestTitle === _.kebabCase(post.title))
+      res.render('post', { 
+        title: post.title, 
+        content: post.content 
+      })
+  
     })
-
   })
+  
 
 
 })
@@ -64,13 +82,21 @@ app.get('/posts/:postName', (req, res) => {
 // postReq
 
 app.post('/compose', (req, res) => {
-  let post = {
+  let post = new Post ({
     title: req.body.postTitle,
     content: req.body.postBody,
     link: `posts/${_.kebabCase(req.body.postTitle)}`
-  }
-  posts.push(post);
-  res.redirect('/');
+  })
+
+  post.save(err=>{
+    if(!err) {
+      console.log('New post has been added to DB');
+      res.redirect('/')
+    }
+    else {
+      console.log(err);
+    }
+  })
 })
 
 app.listen(3000, function () {
