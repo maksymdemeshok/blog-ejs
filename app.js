3d4a3e3bb77cb05a79b750e7ed76b206877010ba
Maksym Demeshok <maksymdemeshok@MacBook-Pro-Maksym.local>
2021-11-16 22:57:52 +0100

    render post based on ID

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const ejs = require("ejs");
 const _ = require('lodash');
 const mongoose = require('mongoose');
 
-mongoose.connect(`mongodb://localhost:27017/blogDB`); 
+mongoose.connect(`mongodb://localhost:27017/blogDB`);
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -22,10 +22,10 @@ app.use(express.static("public"));
 const postsSchema = mongoose.Schema({
   title: String,
   content: String,
-  link: String
+  // link: String
 })
 
-const Post = new mongoose.model('Post', postsSchema); 
+const Post = new mongoose.model('Post', postsSchema);
 
 
 
@@ -37,11 +37,11 @@ const Post = new mongoose.model('Post', postsSchema);
 
 app.get('/', (req, res) => {
 
-  Post.find((err, posts)=>{
+  Post.find((err, posts) => {
     res.render('home', { homeStartingContent: homeStartingContent, posts: posts });
   })
-  
-  
+
+
 
 })
 
@@ -57,22 +57,21 @@ app.get('/compose', (req, res) => {
   res.render('compose')
 })
 
-app.get('/posts/:postName', (req, res) => {
+app.get('/posts/:postID', (req, res) => {
 
+  let postID = req.params.postID;
 
-  let requestTitle = _.kebabCase(req.params.postName);
+  Post.findOne({ _id: postID }, (err, post) => {
 
-  Post.find((err, posts)=>{
-    posts.forEach(post => {
-      if(requestTitle === _.kebabCase(post.title))
-      res.render('post', { 
-        title: post.title, 
-        content: post.content 
+    if (!err) {
+      res.render('post', {
+        title: post.title,
+        content: post.content
       })
-  
-    })
+    } else {
+      console.log(err);
+    }
   })
-  
 
 
 })
@@ -82,14 +81,14 @@ app.get('/posts/:postName', (req, res) => {
 // postReq
 
 app.post('/compose', (req, res) => {
-  let post = new Post ({
+  let post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody,
-    link: `posts/${_.kebabCase(req.body.postTitle)}`
+    // link: `posts/${_.kebabCase(req.body.postTitle)}`
   })
 
-  post.save(err=>{
-    if(!err) {
+  post.save(err => {
+    if (!err) {
       console.log('New post has been added to DB');
       res.redirect('/')
     }
